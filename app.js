60ae43e4e3dda3e71d01e8b66746aa4c734dde7a
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-07 23:04:37 +0530

    Set up basic boilerplate and install mongoose,bodyparser

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const mongoose = require('mongoose'),
+bodyParser = require('body-parser'),
+express = require('express'),
+app = express(),
+port = 3000;
 
-const app = express();
-const _port = 3000;
+mongoose.connect('mongodb://localhost:27017/restful_blog_app',{useNewUrlParser:true});
 
-app.get('/', (req,res) => {
-	res.send('First app');
-})
+// Mongoose/Model config
+let blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
 
-app.get('*', (req,res) => {
-	res.send('Wrong page bub');
-})
+let Blog = mongoose.model('Blog',blogSchema);
 
-app.listen(_port, () => {
-	console.log('Server started');
-})
+// App Config
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// RESTful Routes
+
+app.listen(port,() => console.log('Server started'));
+
